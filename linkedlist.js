a13702f69050f876e90feea0489f82fef5d50690
Mufti Mehedi Hasan <130230722+muftimehedihasan@users.noreply.github.com>
2024-09-03 01:47:51 +0600

    new commit

--- linkedlist.js
+++ linkedlist.js
@@ -7,7 +7,18 @@ class LinkedList{
         this.tail = this.head
         this.length = 1;
     }
+    append(data){
+        const newNode ={
+            value: data,
+            next: null,
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
+
+
 }
 
-const myList = new LinkedList(10)
-console.log(myList)
+const myList = new LinkedList(10);
+myList.append(16);
+console.log(myList);
